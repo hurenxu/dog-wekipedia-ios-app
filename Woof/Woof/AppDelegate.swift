3342d74ef88694817f1b30a3cd4a620770685aeb
Jorx11 <jo.r.xu@outlook.com>
2017-01-31 18:13:06 -0800

    Fixed Appdelegate.swift

--- Woof/Woof/AppDelegate.swift
+++ Woof/Woof/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,10 +19,19 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+        
+        
         FIRApp.configure()
         return true
     }
     
+    func application(_ app: UIApplication, open url: URL, options:[UIApplicationLaunchOptionsKey: Any] = [:]) -> Bool {
+        
+        let handled = FBSDKApplicationDelegate.sharedInstance().application(app, open: url, sourceApplication: options[UIApplicationLaunchOptionsKey.sourceApplication] as! String!, annotation: options[UIApplicationLaunchOptionsKey.annotation])
+        return handled
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
---
+++ Woof/Woof/LoginViewController.swift
@@ -0,0 +1,25 @@
+//
+//  LoginViewController.swift
+//  Woof
+//
+//  Created by Joanna Xu on 1/31/17.
+//  Copyright © 2017 Woof. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class LoginViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let loginButton = FBSDKLoginButton()
+        loginButton.center = view.center
+        
+        view.addSubview(loginButton)
+
+    }
+    
+
+}
