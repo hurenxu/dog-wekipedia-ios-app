5a47bf286514b583eeb23ffa2575a35e05a407ee
Hi <sil142@ucsd.edu>
2017-02-16 19:31:40 -0800

    modified DAO

--- Woof/Woof/DataAccessObject.swift
+++ Woof/Woof/DataAccessObject.swift
@@ -3,6 +3,7 @@
 //  Woof
 //
 //  Created by Ciel Slytherin on 2017/2/12.
+//  Modified by Siya Li on 2017/02/16
 //  Copyright © 2017年 Woof. All rights reserved.
 //
 
@@ -30,5 +31,31 @@ class DataAccessObject {
         let ref = FIRDatabase.database().reference()
         
         ref.child("User Profile").child("My Liked Dog").setValue(["name": name, "key": key])
+    }
+    
+    /*the following method is for temporary explaination of retrieving data,
+     may be moved to controller for implemenation*/
+    func viewDog(name: String, key: String) {
+        
+        //Attach a listener to receive updates
+        ref.observe(.value, with: { snapshot in
+            var newDogs: [Dog] = []
+            for item in snapshot.children {
+                let dog = Dog(snapshot: item as! FIRDataSnapshot)
+                newDogs.append(dog)
+            }
+            /* the following implementation may be applied to viewController */
+            self.dogs = newDogs // this updates the field 'dogs' under the user referenced
+            //self.view.reloadData()
+        })
+    
+    }
+    /* the following method is intended to be implemented in some tableViewController,
+       which is to delete a dog in cell */
+    func tableView(_tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete{
+            let dogToBeDeleted = dog[indexPath.row]
+            dogToBeDeleted.ref?.removeValue()
+        }
     }
 }
