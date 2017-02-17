ecb0e87f3c970cd2ad07e0cbfec03a77a24ed31d
Hi <sil142@ucsd.edu>
2017-02-16 20:51:10 -0800

    dog class modified

--- Woof/Woof/Dog.swift
+++ Woof/Woof/Dog.swift
@@ -1,13 +1,87 @@
 import Foundation
 
 class Dog{
-
-	let name: String
+    
+    let dogID: Int
+	var name: String
     let breed: Breed
-
-	init(name: String){
+    var age: Int
+    let gender: String
+    let birthDate: Date
+    var vaccination: Date
+    var color: String
+    var description: String
+    var image: String
+    
+    
+    init(name: String, breed: Breed, birthDate: Date, age: int, gender: String, description: String){
 		self.name = name
-        breed = Breed(breedName: "")
+        self.breed = breed
+        self.age = age
+        self.gender = gender
+        self.birthDate = birthDate
+        self.description = description
+        
 	}
+    func getBreed() -> Breed{
+        return breed
+    }
+    
+    func getBreedName() -> String {
+        return breed.getBreedName()
+    }
+    
+    func getDogID() -> Int {
+        return self.dogID
+    }
+    func getName() -> String{
+        return name
+    }
+    func getAge() -> Int{
+        return age
+    }
+    func getGender() -> String{
+        return gender
+    }
+    func getBirthDate() -> Date{
+        return birthDate
+    }
+    func getVaccination() -> Date{
+        return vaccination
+    }
+    func getColor() -> String{
+        return color
+    }
+    func getDescription() -> String{
+        return description
+    }
+    func getImage() -> String{
+        return image
+    }
+    
+    func setAge(age: Int){
+        self.age = age
+    }
+    
+    func setImage(imageUrl: String){
+        self.image = imageUrl
+    }
+    
+    func setColor( color: String){
+        self.color = color
+    }
+    
+    func setVaccinationDate( vacDate: Date) {
+        self.vaccination = vacDate
+    }
+
+    func setDescription( des: String) {
+        self.description = des
+    }
+    
+    func addDogProfileEntry() {
+        let dao = DataAccessObject()
+        dao.addDog(self)
+    }
 
 }
