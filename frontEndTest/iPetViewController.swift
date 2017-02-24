3ab229ff9ef56d09cc2ad9d8f7b2cb9914ca116b
Meiyi <hemeiyi0225@gmail.com>
2017-02-23 18:07:29 -0800

    add picking image function for user profile

--- frontEndTest/iPetViewController.swift
+++ frontEndTest/iPetViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class iPetViewController: UIViewController {
+class iPetViewController: UIViewController, UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    //var changeProfile:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,10 +21,34 @@ class iPetViewController: UIViewController {
         print("iPet Page loaded")
         
         
-        let userImg = profileImg(name: "BlackEmptyDog")
+//        if( changeProfile == 0 ){
+//            let userImg = profileImg(name: "BlackEmptyDog")
+//            userImg.setRounded()
+//            self.view.addSubview(userImg)
+//            ImageViewConstraints(Img: userImg)
+//        }else{
+//            let userImg = profileImgContainer
+//            userImg.setRounded()
+//            self.view.addSubview(userImg)
+//            ImageViewConstraints(Img: userImg)
+//            super.viewDidLoad()
+        //}
+        
+        
+        let userImg = profileImgContainer
+        userImg.setRounded()
         
         self.view.addSubview(userImg)
         ImageViewConstraints(Img: userImg)
+        
+        
+        let editProfButton:UIButton = UIButton(frame: CGRect(x: 100, y: 400, width: 100, height: 50))
+        editProfButton.backgroundColor = .black
+        editProfButton.setTitle("click here", for: .normal)
+        view.addSubview(editProfButton)
+        EditProfileImageButtonConstraints(Button: editProfButton)
+        
+        
         
         let label = UILabel(frame: CGRect(x: 200, y: 200, width: 200, height: 250))
         label.center = CGPoint(x: 180, y: 250)
@@ -33,32 +59,87 @@ class iPetViewController: UIViewController {
         label.font = label.font.withSize(30)
         view.addSubview(label)
         
-        
     }
     
     func profileImg(name:String) -> UIImageView{
-        let Img = UIImageView()
+        let dogImg = UIImageView()
         
         if(UIImage(named:name)?.size == nil){
-            Img.image = UIImage(named:"Yorkshire")
+            dogImg.image = UIImage(named:"Yorkshire")
         }else{
-            Img.image = UIImage(named:name)
+            dogImg.image = UIImage(named:name)
         }
         
-        Img.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
-        Img.translatesAutoresizingMaskIntoConstraints = false
+        dogImg.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        dogImg.translatesAutoresizingMaskIntoConstraints = false
         //You need to call this property so the image is added to your view
-        return Img
+        return dogImg
     }
+
+    
+    
+    var profileImgContainer: UIImageView = {
+        let ImgView = UIImageView()
+        let Img = UIImage(named: "BlackEmptyDog")
+        
+        ImgView.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        ImgView.translatesAutoresizingMaskIntoConstraints = false
+        
+        return ImgView
+    }()
     
     func ImageViewConstraints(Img: UIImageView) {
         Img.widthAnchor.constraint(equalToConstant: 100).isActive = true
         Img.heightAnchor.constraint(equalToConstant: 100).isActive = true
         Img.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        Img.topAnchor.constraint(equalTo:topLayoutGuide.bottomAnchor, constant: 50.0).isActive = true
+    }
+    
+    
+    
+    // add constraint to button
+    // add a gesture recognizer to button
+    func EditProfileImageButtonConstraints(Button: UIButton){
+        Button.topAnchor.constraint(equalTo: view.topAnchor, constant:180.0).isActive = true
+        Button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        Button.translatesAutoresizingMaskIntoConstraints = false
+        Button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        //Button.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleSelectProfileImageView)))
+    }
+    
+    func buttonAction(sender: UIButton!){
+        sender.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleSelectProfileImageView)))
+        //changeProfile = 1
+    }
+    
+    
+    func handleSelectProfileImageView(){
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+    }
+    
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        var chosenImageFromPicker: UIImage?
+        if let editedImage = info["UIImagePickerControllerOriginalImage"] as? UIImage{
+            chosenImageFromPicker = editedImage
+        }else if let originalImg = info["UIImagePickerControllerOriginalImage"] as? UIImage{
+            chosenImageFromPicker = originalImg
+        }
         
         
-        Img.topAnchor.constraint(equalTo:topLayoutGuide.bottomAnchor, constant: 30.0).isActive = true
-        
+        if let chosenImage = chosenImageFromPicker{
+            profileImgContainer.image = chosenImage
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
     }
 
     
@@ -68,5 +149,15 @@ class iPetViewController: UIViewController {
         tabBarItem = UITabBarItem(title: "iPet", image: UIImage(named: "iPet"), tag: 1)
         tabBarItem.badgeValue = "3"
     }
-
+    
 }
+
+extension UIImageView {
+    
+    func setRounded() {
+        let radius = self.frame.width / 2
+        self.layer.cornerRadius = radius
+        self.layer.masksToBounds = true
+    }
+}
+
