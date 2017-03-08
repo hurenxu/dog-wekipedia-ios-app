a753f93e023244c02c30d0322d96bef4d7fa989a
Shifan Zhang <shifanzhang@Shifans-MacBook-Pro.local>
2017-03-07 23:21:12 -0800

    haozi2

--- frontEndTest/OwnedDogCell.swift
+++ frontEndTest/OwnedDogCell.swift
@@ -15,7 +15,8 @@ class OwnedDogCell: UICollectionViewCell {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        self.backgroundColor = UIColor.white
+        self.backgroundColor = UIColor(red: 255.0/255.0, green: 228.0/255.0, blue: 196.0/255.0, alpha: 1)
+
         imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: frame.size.width, height: frame.size.height*2/3))
         imageView?.contentMode = UIViewContentMode.scaleAspectFit
         self.imageView?.layer.cornerRadius = self.imageView!.frame.size.width / 2
@@ -24,12 +25,14 @@ class OwnedDogCell: UICollectionViewCell {
         
         imageView?.isUserInteractionEnabled = true
         
-        textLabel = UILabel(frame: CGRect(x: 0, y: (imageView?.frame.size.height)!, width: frame.size.width, height: frame.size.height/2))
-        textLabel?.font = UIFont.systemFont(ofSize: UIFont.smallSystemFontSize)
+        textLabel = UILabel(frame: CGRect(x: 0, y: (imageView?.frame.size.height)! - 16, width: frame.size.width, height: frame.size.height/2))
+        textLabel?.font = UIFont(name: "Noteworthy", size: 14)
         textLabel?.textAlignment = .center
         //textLabel?.text = "test"
         textLabel?.textColor = UIColor.black
         contentView.addSubview(textLabel!)
+        
+    
         
         
     }
