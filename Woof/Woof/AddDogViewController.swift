b4bca35cd532115ce8a17d557840d412e606caed
Meiyi He <hemeiyi0225@gmail.com>
2017-03-10 20:57:02 -0800

    add progile

--- Woof/Woof/AddDogViewController.swift
+++ Woof/Woof/AddDogViewController.swift
@@ -290,6 +290,8 @@ class AddDogViewController: UIViewController, UINavigationBarDelegate, UIImagePi
         
         ageLabel.text = "2"
         genderLabel.text = "female"
+        
+        
         let updateMyNewDog = Dog(dogID: dogID, name: nameLabel.text!, breed: breed, birthDate: Date(), age: ageLabel.text!, gender: genderLabel.text!, vaccination: Date(), color: "", description: "", image: "")
         Functionalities.myUser?.updateDog(dog: updateMyNewDog)
 
